840537ff7404a706804482d1a0cf3f1cf67a0f8d
NamkyuHan <rksh333@naver.com>
2024-05-21 18:35:21 +0900

    fix: local test text add

--- src/main/java/com/base/boilerplate/BoilerPlateApplication.java
+++ src/main/java/com/base/boilerplate/BoilerPlateApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,14 @@ public class BoilerPlateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BoilerPlateApplication.class, args);
+    }
+
+    @RestController
+    public class WelcomeController {
+        @GetMapping("/")
+        public String welcome() {
+            return "welcome-ec2";
+        }
     }
 
 }
